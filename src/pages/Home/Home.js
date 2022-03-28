4080888989fb53a063f07b27dfa0ebe468004303
bananabread08 <rmkfermin@gmail.com>
2022-03-28 10:21:52 +0800

    add context to Outlet for other components' consumption

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,11 @@
+import React, { useState, useEffect } from 'react';
 import Servers from '../../components/Servers/Servers';
 import Channels from '../../components/Channels/Channels';
-// import ChatPanel from '../../components/ChatPanel/ChatPanel';
 import UserPanel from '../../components/UserPanel/UserPanel';
 import styled from 'styled-components';
+import { useUserAuth } from '../../context/UserAuthContext';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../firebase-config';
 
 import { Outlet } from 'react-router-dom';
 const Wrapper = styled.div`
@@ -10,12 +13,25 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
 `;
-export default function Home({ toggleTheme, servers }) {
+export default function Home({ toggleTheme, servers, serverChannels }) {
+  const [currentUser, setCurrentUser] = useState([]);
+  const { user } = useUserAuth();
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, `users/${user.uid}`), (snapshot) => {
+      setCurrentUser(snapshot.data());
+    });
+    return () => unsub();
+  }, [user]);
+
   return (
     <Wrapper>
       <Servers toggleTheme={toggleTheme} servers={servers}></Servers>
-      <Channels servers={servers} />
-      <Outlet />
+      <Channels
+        servers={servers}
+        serverChannels={serverChannels}
+        currentUser={currentUser}
+      />
+      <Outlet context={[currentUser, setCurrentUser]} />
       <UserPanel />
     </Wrapper>
   );
