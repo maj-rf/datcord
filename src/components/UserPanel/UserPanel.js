c0fc4423a3ba6d4cd0d1f01434aef51fabb67675
bananabread08 <rmkfermin@gmail.com>
2022-04-01 15:59:00 +0800

    remove onSnapshot listener

--- src/components/UserPanel/UserPanel.js
+++ src/components/UserPanel/UserPanel.js
@@ -1,19 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { UserPanelSection } from './UserPanel.style';
 import User from '../User/User';
-import { onSnapshot, collection } from 'firebase/firestore';
-import { db } from '../../firebase-config';
-function UserPanel() {
-  const [currentUsers, setCurrentUsers] = useState([]);
-  useEffect(() => {
-    const unsub = onSnapshot(collection(db, 'users'), (snapshot) => {
-      setCurrentUsers(snapshot.docs.map((doc) => doc.data()));
-    });
-    return unsub;
-  }, []);
 
-  const online = [...currentUsers].filter((user) => user.isOnline);
-  const offline = [...currentUsers].filter((user) => user.isOnline === false);
+function UserPanel({ allUsers }) {
+  const online = [...allUsers].filter((user) => user.isOnline);
+  const offline = [...allUsers].filter((user) => user.isOnline === false);
   return (
     <UserPanelSection>
       <div>
