46e9e367fec9441f99a50ccfc5ca931e31497e05
bananabread08 <rmkfermin@gmail.com>
2022-04-01 10:11:08 +0800

    implement update name functionality -add form add style

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import { ProfileWrapper } from './Profile.style';
-function Profile({ handleProfileView, handleLogOut }) {
+import React, { useState } from 'react';
+import { ProfileWrapper, ProfileInner } from './Profile.style';
+import User from '../User/User';
+import { StyledButton } from '../../styles/sharedStyles';
+import { useUserAuth } from '../../context/UserAuthContext';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../../firebase-config';
+function Profile({ handleProfileView, handleLogOut, currentUser }) {
+  const { user } = useUserAuth();
+  const [isEditing, setIsEditing] = useState(false);
+  const [input, setInput] = useState('');
+
+  async function updateName(e) {
+    e.preventDefault();
+    const userRef = doc(db, 'users', user.uid);
+    try {
+      await updateDoc(userRef, {
+        name: input,
+      });
+      setIsEditing(false);
+      setInput('');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function handleChange(e) {
+    setInput(e.target.value);
+  }
   return (
     <ProfileWrapper>
-      <h1>Profile</h1>
-      <button onClick={handleProfileView}>Go back</button>
-      <button onClick={handleLogOut}>Log Out</button>
+      <ProfileInner>
+        <h1>Profile</h1>
+        <User x={currentUser} primary />
+        <div>
+          {isEditing ? (
+            <form onSubmit={updateName}>
+              <input placeholder="Name..." onChange={handleChange}></input>
+            </form>
+          ) : (
+            <>
+              <p>Username: {currentUser?.name}</p>
+              <button type="button" onClick={() => setIsEditing(true)}>
+                Edit
+              </button>
+            </>
+          )}
+          <p>Email: {currentUser?.email}</p>
+        </div>
+        <StyledButton submit onClick={handleProfileView}>
+          Go back
+        </StyledButton>
+        <StyledButton submit onClick={handleLogOut}>
+          Log Out
+        </StyledButton>
+      </ProfileInner>
     </ProfileWrapper>
   );
 }
--- src/components/Profile/Profile.style.js
+++ src/components/Profile/Profile.style.js
@@ -1,9 +1,23 @@
 import styled from 'styled-components';
 
 const ProfileWrapper = styled.section`
-  z-index: 2;
   height: 100vh;
-  width: 100vw;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-export { ProfileWrapper };
+const ProfileInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 800px;
+  background-color: ${(props) => props.theme.text};
+  color: ${(props) => props.theme.body};
+  padding: 1.5rem;
+  gap: 1rem;
+  border-radius: 7px;
+`;
+
+export { ProfileWrapper, ProfileInner };
