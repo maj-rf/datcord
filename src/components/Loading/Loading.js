bc40de45bdb88e1be5ac3faa4aa8fc7ca2900b24
bananabread08 <rmkfermin@gmail.com>
2022-04-10 09:37:18 +0800

    create Loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }`;
+const Spinner = styled.div`
+  border: 10px solid ${(props) => props.theme.body};
+  border-top: 10px solid ${(props) => props.theme.text};
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  animation: ${spin} 1s linear infinite;
+`;
+function Loading() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Spinner />
+    </div>
+  );
+}
+
+export default Loading;
